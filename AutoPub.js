5a85af8c2e554c8786b9b56bf63dd9585e1c4398
kalimans1 <kanadalipes@gmail.com>
2025-08-17 10:26:17 +0300

    Update AutoPub.js

--- AutoPub.js
+++ AutoPub.js
@@ -1,4 +1,16 @@
 const { AutoPub, Webhook } = require("./src/Structures/AutoPub");
 const client = new AutoPub();
 client.webhook = new Webhook();
+
 client.start();
+
+// --- Render deploy fix: Fake web server ---
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("OK");
+}).listen(port, () => {
+  console.log(`Fake web service running on port ${port}`);
+});
